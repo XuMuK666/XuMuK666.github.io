648292d5f2be87bcbec3debb4104a43f575aab65
XuMuK666 <dedelya666@gmail.com>
2020-04-26 22:15:07 +0300

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -6,7 +6,14 @@
  importScripts('https://www.gstatic.com/firebasejs/7.14.0/firebase-messaging.js');
 
 firebase.initializeApp({
-    messagingSenderId: '390667823333'
-});
+    apiKey: "AIzaSyBWh4lRntilVk-XYablX8iv6TQ7swkXy5s",
+    authDomain: "github-ae841.firebaseapp.com",
+    databaseURL: "https://github-ae841.firebaseio.com",
+    projectId: "github-ae841",
+    storageBucket: "github-ae841.appspot.com",
+    messagingSenderId: "390667823333",
+    appId: "1:390667823333:web:2d1fff9afa969b7d044c0c",
+    measurementId: "G-WFKYCXHV4S"
+  });
 
 const messaging = firebase.messaging();
